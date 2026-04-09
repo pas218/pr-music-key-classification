4d93a56e08ffc411a52bf443a1b8d40be89567f7
Kushal Parekh <kup9@pitt.edu>
2026-04-09 13:18:28 -0400

    [#2] create wav files from all midi and soundfonts

--- midi_to_wav.sh
+++ midi_to_wav.sh
@@ -1,9 +1,29 @@
-for file in soundfonts/*; do
-    sffilename="${file##*/}"
-    sffilename="${sffilename%%.*}"
-    # echo "$file"
-    # echo "$sffilename"
-    fluidsynth -n -i -F sound_outputs/output_$sffilename.wav $file ./POP909-Dataset/POP909/001/001.mid
+for midifile in POP909-Dataset/POP909/*; do
+    if [ -d "$midifile" ]; then
+        echo "$midifile"
+        midifilenum="${midifile##*/}"
+        echo "$midifilenum"
+
+        for file in soundfonts/*; do
+            sffilename="${file##*/}"
+            sffilename="${sffilename%%.*}"
+            # echo "$file"
+            # echo "$sffilename"
+            fluidsynth -n -i -F "sound_outputs/output_${midifilenum}_${sffilename}.wav" $file ./POP909-Dataset/POP909/$midifilenum/$midifilenum.mid
+        done
+
+
+
+    fi
+        
 done
+
+# for file in soundfonts/*; do
+#     sffilename="${file##*/}"
+#     sffilename="${sffilename%%.*}"
+#     # echo "$file"
+#     # echo "$sffilename"
+#     fluidsynth -n -i -F sound_outputs/output_$sffilename.wav $file ./POP909-Dataset/POP909/001/001.mid
+# done
 
 # fluidsynth -n -i -F sound_outputs/output.wav soundfonts/fzero.sf2 ./POP909-Dataset/POP909/001/001.mid
