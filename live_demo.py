b093cfc80799da6c917d222f93a8d4ec509a7165
pas218 <pas218@pitt.edu>
2026-04-20 11:04:56 -0400

    Live demo is working. Also had to re-adjust the CNN_LSTM class because of some weird sizing issues that occured between the training and live demo.

--- live_demo.py
+++ live_demo.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as pltimport
 import torch
 import torchaudio
 import torchaudio.transforms as T
+import matplotlib.pyplot as plt
+import soundfile as sf
+from torchvision.transforms import v2
 from CNN_LSTM import CNN_LSTM
+from PIL import Image
+from key_classfn_utilities import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 WAV_FILENAME = "recording.wav"
 SPECT_FILENAME = "spect.png"
-WEIGHTS_FILENAME = "weights.pt2"
+WEIGHTS_FILENAME = "./test_weights.pth"
+MODEL_FILENAME = "./model_weights_83acc.pth"
 
 # Configuration
 freq = 44100  # Sampling frequency
@@ -57,13 +63,31 @@ plt.savefig(SPECT_FILENAME, bbox_inches='tight', transparent=True, pad_inches=0.
 plt.close()
 
 
-model = CNN_LSTM()
-model.load_state_dict(WEIGHTS_FILENAME)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = CNN_LSTM(num_classes=24, num_filters=16, num_hidden_units=128).to(device)
+model.load_state_dict(torch.load(WEIGHTS_FILENAME))
+##model = torch.load(MODEL_FILENAME)
+#print(model)
+#model = CNN_LSTM()
+#model.load_state_dict(torch.load(WEIGHTS_FILENAME, weights_only=True))
 
 model.eval()
 
-output = model(SPECT_FILENAME)
+transform = v2.Compose([
+    v2.ToImage(),                              # convert PIL → tensor first
+    v2.Grayscale(num_output_channels=1),
+    v2.Resize((450, 600), antialias=True),
+    v2.ToDtype(torch.float32, scale=True)
+])
 
-print(f'The model prediction is {output}.')
+img = Image.open(SPECT_FILENAME).convert("RGB")  # ensure 3 channels
+img_tensor = transform(img)
+img_tensor = img_tensor.unsqueeze(0).to(device)
 
+# Run inference
+with torch.no_grad():
+    output = model(img_tensor)
+    predicted_class = torch.argmax(output, dim=1).item()
+
+print(f'The model prediction is {predicted_class}.')
 
