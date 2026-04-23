fixed filters and hidden units, search dropout

CNN_LSTM.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CNN_LSTM(nn.Module):
-    def __init__(self, num_classes=24, num_filters=16, num_hidden_units=128):
+    def __init__(self, num_classes=24, num_filters=128, num_hidden_units=256, dropout_rate=0):
         super(CNN_LSTM, self).__init__()
         
         # -------- Convolutional Blocks --------
@@ -31,6 +31,10 @@ class CNN_LSTM(nn.Module):
             batch_first=True
         )
 
+        # ------- Dropout ----------
+        self.dropout_conv = nn.Dropout2d(p=dropout_rate)
+        self.dropout_lstm = nn.Dropout(p=dropout_rate)
+
         # -------- Fully Connected --------
         self.fc = nn.Linear(num_hidden_units, num_classes)
 
@@ -43,9 +47,11 @@ class CNN_LSTM(nn.Module):
         
         # Conv Block 3
         x = self.pool3(F.relu(self.bn3(self.conv3(x))))
+        x = self.dropout_conv(x)
         
         # Conv Block 4
         x = self.pool4(F.relu(self.bn4(self.conv4(x))))
+        x = self.dropout_conv(x)
 
         # x shape: (B, C, H, W)
         B, C, H, W = x.size()
@@ -69,6 +75,8 @@ class CNN_LSTM(nn.Module):
 
         # Take last time step (OutputMode="last")
         x = x[:, -1, :]
+
+        x = self.dropout_lstm(x)
 
         # Fully connected
         x = self.fc(x)

key-classification.py
@@ -32,9 +32,11 @@ transform = v2.Compose([
 ])
 
 
-def main(num_filters, num_hidden_units):
+def main(dropout_rate):
     batch_size = 16
     num_epochs = 20
+    num_filters = 128
+    num_hidden_units = 256
 
     dataset = cd.CustomImageDataset(annotations_file=f'./dataset/labels.csv', img_dir=f'./dataset/spects', num_soundfonts=NUM_SOUNDFONTS, soundfont_map=soundfont_map, transform=transform)
 
@@ -45,14 +47,14 @@ def main(num_filters, num_hidden_units):
 
     # Initialize model, loss, and optimizer
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = CNN_LSTM(num_classes=24, num_filters=num_filters, num_hidden_units=num_hidden_units).to(device)
+    model = CNN_LSTM(num_classes=24, num_filters=num_filters, num_hidden_units=num_hidden_units, dropout_rate=dropout_rate).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=.00075)
 
     # Training Loop
     model.train() # Set model to training mode
     for epoch in range(num_epochs):
-        train_loop = tqdm(train_loader, leave=True, disable=True)
+        train_loop = tqdm(train_loader, leave=True, disable=False)
         for batch_idx, (images, labels) in enumerate(train_loop):
             images, labels = images.to(device), labels.to(device)
             
@@ -102,14 +104,12 @@ def main(num_filters, num_hidden_units):
 
 
 if __name__ == '__main__':
-    filters = [32, 64]
-    hiddenunits = [32, 64, 128, 256, 512]
-    
-    for num_filters in filters:
-        for num_hu in hiddenunits:
-            curr_accuracy = main(num_filters=num_filters, num_hidden_units=num_hu)
 
-            print(f"Model Acc: {curr_accuracy}, FILTERS: {num_filters}, HIDDENUNITS: {num_hu}")
+    Dropouts = [.2, .3, .5]
+
+    for dropout in Dropouts:
+        curr_accuracy = main(dropout_rate=dropout)
+        print(f"Model Acc: {curr_accuracy}, Dropout Rate: {dropout}")
             
 
     